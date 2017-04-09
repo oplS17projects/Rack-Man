58635973308b63e24edd448af9b83415be440c9c
Kevin Fossey <lictlesquix@gmail.com>
2017-04-09 18:09:42 -0400

    Added Ghost Logic
    added logic to have the ghost follow the player around the screen. Also added logic to check the ghosts position and end the game if the player touches the ghost

--- rackman.rkt
+++ rackman.rkt
@@ -107,24 +107,73 @@
     ;(if(equal? (remainder OPEN 28) 0) ;
     ;   (begin (set! OPEN 0) (set! RACKMAN (bitmap/file "./rackman_right_o.png")))
     ;   (begin (set! OPEN (add1 OPEN)) (set! RACKMAN (bitmap/file "./rackman_right_c.png"))))
-    (cond ((not (list? w)) w)
-          ;HANDLE ARROW KEY COMMANDS
-          ((equal? LEFT #t)
-             (if (<= (car w) 11) (list (+ (car w) 0) (cadr w) (third w) (fourth w)) (list (- (car w) SPEED) (cadr w) (third w) (fourth w))))
-          ((equal? RIGHT #t)
-             (if (>= (car w) 489) (list (+ (car w) 0) (cadr w) (third w) (fourth w)) (list (+ (car w) SPEED) (cadr w) (third w) (fourth w))))
-          ((equal? DOWN #t)
-             (if (>= (cadr w) 489) (list (car w) (+ (cadr w) 0) (third w) (fourth w)) (list (car w) (+ (cadr w) SPEED) (third w) (fourth w))))
-          ((equal? UP #t)
-             (if (<= (cadr w) 11) (list (car w) (+ (cadr w) 0) (third w) (fourth w)) (list (car w) (- (cadr w) SPEED) (third w) (fourth w))))
-          (else w))))
+    (if (equal? w 0)
+        w
+        (cond ((not (list? w)) w)
+              ;HANDLE ARROW KEY COMMANDS
+              ((equal? LEFT #t)
+               (if (<= (car w) 11) 
+                   (list (+ (car w) 0) (cadr w) (ghost "x" w) (ghost "y" w))
+                   (list (- (car w) SPEED) (cadr w) (ghost "x" w) (ghost "y" w))))
+              ((equal? RIGHT #t)
+               (if (>= (car w) 489)
+                   (list (+ (car w) 0) (cadr w) (ghost "x" w) (ghost "y" w))
+                   (list (+ (car w) SPEED) (cadr w) (ghost "x" w) (ghost "y" w))))
+              ((equal? DOWN #t)
+               (if (>= (cadr w) 489)
+                   (list (car w) (+ (cadr w) 0) (ghost "x" w) (ghost "y" w))
+                   (list (car w) (+ (cadr w) SPEED) (ghost "x" w) (ghost "y" w))))
+              ((equal? UP #t)
+               (if (<= (cadr w) 11)
+                   (list (car w) (+ (cadr w) 0) (ghost "x" w) (ghost "y" w))
+                   (list (car w) (- (cadr w) SPEED) (ghost "x" w) (ghost "y" w))))
+              (else w)))))
 
+; GHOST
+(define (ghost d w)
+  (if (equal? d "x")
+      (if (< (third w) (first w))
+          (+ (third w) 1)
+          (- (third w) 1))
+      (if (< (fourth w) (second w))
+          (+ (fourth w) 1)
+          (- (fourth w) 1))))
+
+; GAME OVER MAN! GAME OVER!
+(define (game-over w)
+  (cond ((equal? w 0) #f)
+        ((equal? (check-ghost (list (first w) (second w)) (list (third w) (fourth w))) #t) #t)
+        (else #f)))
+
+; check the position of the ghost relative to rack-man
+; r -> '(x y) for rack-man
+; g -> '(x y) for ghost
+; To account for the size of rack-man and the ghost, it recursively compares the next 10 values of the x/y
+(define (check-ghost r g)
+  (define (check-iter r_pos g_pos count)
+    (if (> count 0)
+        (if (equal? r_pos g_pos)
+            #t
+            (check-iter r_pos (add1 g_pos) (sub1 count)))
+        #f))
+  (cond ((and (equal? (check-iter (first r) (first g) 10) #t) (equal? (check-iter (second r) (second g) 10) #t))  #t)
+        (else #f)))
+
+; NOT COMPLETE YET 
+; Dipsplay Local high-score
+(define (high-score w)
+  (place-image (text "GAME OVER MAN! GAME OVER!" 24 "white")
+                   250 400 ; x y
+                   (place-image SPLASH
+                                250 250
+                                (rectangle 500 500 "outline" "black"))))
 ; WORLD
 (big-bang 0;'(50 50 75 75)
           (on-tick tick-handler)
           (to-draw myWorld)
           (on-key react)
-          (name "Rack-Man v0.1"))
+          (name "Rack-Man v0.1")
+          (stop-when game-over high-score))
 
 
 
